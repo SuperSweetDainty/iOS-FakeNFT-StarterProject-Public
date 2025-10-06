fe0bd6acbaa45cafdbd7daf6f7fb933a0c5d1e31
R Kolos <rkolos@MacBook-Pro-R-2.local>
2025-10-06 12:57:46 +0200

    Cart, started cart1-3

---
+++ FakeNFT/Scenes /Cart/CartContoller.swift
@@ -0,0 +1,17 @@
+//
+//  CartContoller.swift
+//  FakeNFT
+//
+//  Created by R Kolos on 04/10/25.
+//
+
+import UIKit
+
+class CartContoller: UIViewController {
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
